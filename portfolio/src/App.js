88b54ee5de364c33f66bac4ac9d007011a0d20b7
HP <fatimaaviles1612@gmail.com>
2019-11-14 23:09:47 -0600

    created ract app in portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <h1>Fatima's Portfolio</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
