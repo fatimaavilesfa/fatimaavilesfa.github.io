7d13f2841b5b5b96965c6a6e0f59b20a50588e47
seisvelas <fatima_aviles@ksquare.university>
2020-08-03 13:35:18 -0500

    build react

---
+++ precache-manifest.40152fb29658461f5ff8683346937032.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef071d69b2f1b869673d44ef84533710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531f122fde9195f863ef",
+    "url": "/static/css/main.13f462dc.chunk.css"
+  },
+  {
+    "revision": "79917e5195b74ef28b50",
+    "url": "/static/js/2.5fa83b00.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.5fa83b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531f122fde9195f863ef",
+    "url": "/static/js/main.90a6c1e0.chunk.js"
+  },
+  {
+    "revision": "ad8caf16babc2df44569",
+    "url": "/static/js/runtime-main.e65a3091.js"
+  },
+  {
+    "revision": "08043b877c7bbd226b6e369b5c663f16",
+    "url": "/static/media/aw1.08043b87.png"
+  },
+  {
+    "revision": "aabfac02d0913624a1dfba67e794b5c6",
+    "url": "/static/media/gallery.aabfac02.png"
+  },
+  {
+    "revision": "e73c5719d29c1d0cd7b289072a28ac14",
+    "url": "/static/media/ghibli.e73c5719.png"
+  },
+  {
+    "revision": "68791ff08fef80c632bd0d0d9231752b",
+    "url": "/static/media/purple2.68791ff0.jpg"
+  },
+  {
+    "revision": "c2d184ddf36935d32428b062b04ded77",
+    "url": "/static/media/todo.c2d184dd.png"
+  },
+  {
+    "revision": "f61292665d6196b35196641af111adc6",
+    "url": "/static/media/weatherApp.f6129266.png"
+  }
+]);
